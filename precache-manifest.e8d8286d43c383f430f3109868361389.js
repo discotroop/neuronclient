9a79bf4c3f340202774d5c849f3033dae22ceda4
John Mark Goodson <john.mark.hat@gmail.com>
2020-09-18 10:49:04 -0400

    Updates

---
+++ precache-manifest.e8d8286d43c383f430f3109868361389.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd0e441d0a847d9b3dfe34e7d096b329",
+    "url": "/neuronClient/index.html"
+  },
+  {
+    "revision": "613f530735f90c37e6b7",
+    "url": "/neuronClient/static/css/2.1b4b65d2.chunk.css"
+  },
+  {
+    "revision": "8556871c11406c43b07c",
+    "url": "/neuronClient/static/css/main.4ecb3f0c.chunk.css"
+  },
+  {
+    "revision": "613f530735f90c37e6b7",
+    "url": "/neuronClient/static/js/2.c7a6f023.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/neuronClient/static/js/2.c7a6f023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8556871c11406c43b07c",
+    "url": "/neuronClient/static/js/main.39f0c603.chunk.js"
+  },
+  {
+    "revision": "1c699dbbc50a871ae323",
+    "url": "/neuronClient/static/js/runtime-main.f77510a2.js"
+  },
+  {
+    "revision": "9bcf1dc03ceaea12f1e4d913a33937fc",
+    "url": "/neuronClient/static/media/info.9bcf1dc0.png"
+  }
+]);
